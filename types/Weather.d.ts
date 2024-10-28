3b4f3a4507bcbf4a9e79aa7ba15c3f593b79fef4
Nag729 <nagamaaaaachi@gmail.com>
2024-10-28 21:16:20 +0900

    fix: `fetchWeathre` function

--- types/Weather.d.ts
+++ types/Weather.d.ts
@@ -1,37 +1,19 @@
-export interface WeatherInfo {
-  lat: number;
-  lon: number;
-  timezone: string;
-  timezone_offset: number;
-  hourly: Hourly[] | RowHourly[];
-}
-
-export interface Hourly {
-  dt?: string;
-  temp?: number;
-  pop?: number;
-}
-
-export interface RowHourly {
-  dt?: number;
-  temp?: number;
-  feels_like?: number;
-  pressure?: number;
-  humidity?: number;
-  dew_point?: number;
-  uvi?: number;
-  clouds?: number;
-  visibility?: number;
-  wind_speed?: number;
-  wind_deg?: number;
-  wind_gust?: number;
-  weather?: Weather;
-  pop?: number;
-}
+// NOTE: define only the necessary types
+type ChanceOfRain = `${number | "--"}%`;
 
 export interface Weather {
-  id: number;
-  main?: string;
-  description?: string;
-  icon?: string;
+  title: string;
+  publicTimeFormatted: string;
+  description: {
+    text: string;
+  };
+  forecasts: {
+    dateLabel: "今日" | "明日" | "明後日";
+    chanceOfRain: {
+      T00_06: ChanceOfRain;
+      T06_12: ChanceOfRain;
+      T12_18: ChanceOfRain;
+      T18_24: ChanceOfRain;
+    };
+  }[];
 }
