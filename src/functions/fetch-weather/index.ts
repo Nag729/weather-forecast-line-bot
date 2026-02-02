78ff0fdd997636a3f5a38831826e05226789cf3e
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2026-02-02 18:41:54 +0900

    update: lambda setting

--- src/functions/fetch-weather/index.ts
+++ src/functions/fetch-weather/index.ts
@@ -2,9 +2,10 @@ import { handlerPath } from "@libs/handlerResolver";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
+  timeout: 60,
   events: [
     {
-      schedule: "cron(0 0 * * ? *)", // Run at 00:00 am (UTC) every day
+      schedule: "cron(30 0 * * ? *)", // Run at 09:30 JST (00:30 UTC) every day
     },
   ],
 };
